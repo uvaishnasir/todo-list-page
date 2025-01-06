478455f07646827659125a08025a1889ca2db8bb
Uvaish <uvaish.nasir@handajitech.com>
2025-01-06 20:52:21 +0530

    fix CSS and abb button

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -11,6 +11,7 @@ function Todolist() {
     { id: 3, text: "NOTE #3", completed: false },
   ]);
   const [newTodo, setNewTodo] = useState("");
+  const [showAddBox, setShowAddBox] = useState(false);
   //all handles
   const handleEdit = (id) => {
     const editedText = prompt("Edit your todo:");
@@ -60,17 +61,28 @@ function Todolist() {
             key={todo.id}
             className="d-flex justify-content-between mx-2 my-3 border-bottom"
           >
-            <div>
+            <div className="d-flex justify-content-center align-items-center my-3">
               <input
+                style={{
+                  width: "20px",
+                  height: "20px",
+                }}
                 type="checkbox"
                 checked={todo.completed}
                 onChange={() => handleToggle(todo.id)}
               />
-              <label className={`${todo.completed ? "line-through" : ""} ms-2`}>
+              <label
+                className="mx-2"
+                style={{
+                  textDecoration: todo.completed ? "line-through" : "none",
+                  color: todo.completed ? "gray" : "black",
+                  fontWeight: "bold",
+                }}
+              >
                 {todo.text}
               </label>
             </div>
-            <div>
+            <div className="d-flex justify-content-center align-items-center">
               <button
                 className="btn btn-outline-secondary btn-sm me-1"
                 onClick={() => handleEdit(todo.id)}
@@ -88,14 +100,25 @@ function Todolist() {
         ))}
       </div>
 
-      {/* floating add button*/}
+      {showAddBox && (
+        <div className="input-group mt-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Add a new todo..."
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+          />
+          <button className="btn btn-primary" onClick={handleAdd}>
+            Add
+          </button>
+        </div>
+      )}
+
       <button
         className="btn btn-primary rounded-circle position-fixed"
-        style={{
-          bottom: "20px",
-          right: "400px",
-        }}
-        onClick={handleAdd}
+        style={{ bottom: "20px", right: "400px" }}
+        onClick={() => setShowAddBox(!showAddBox)}
       >
         <i className="bi bi-plus"></i>
       </button>
